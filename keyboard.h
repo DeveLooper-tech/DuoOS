aa11d66b9a57192a7752f4d0c0fe02f3575007dc
DeveLooperTech <szuszantomika@gmail.com>
2026-07-30 12:35:16 +0200

    delete

--- keyboard.h
+++
@@ -1,6 +0,0 @@
-#ifndef DUOOS_KEYBOARD_H
-#define DUOOS_KEYBOARD_H
-
-void keyboard_handler(void);
-
-#endif
--- mouse.h
+++
@@ -1,7 +0,0 @@
-#ifndef DUOOS_MOUSE_H
-#define DUOOS_MOUSE_H
-
-void mouse_init(void);
-void mouse_handler(void);
-
-#endif
